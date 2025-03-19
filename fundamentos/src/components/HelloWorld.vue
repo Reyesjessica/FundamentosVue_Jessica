3ed9e8599870faac016d4598c9b022ff7d0a4c08
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2025-03-18 23:19:32 -0600

    Pie de pagina

--- fundamentos/src/components/HelloWorld.vue
+++ fundamentos/src/components/HelloWorld.vue
@@ -225,6 +225,10 @@
     </div>
   </section>
 
+  
+  <footer style="background-color: black; color:#fff;" class="text-center py-3 ">@DWB-Todos los derechos reservados
+  </footer>
+
 </template>
 
 <style scoped>

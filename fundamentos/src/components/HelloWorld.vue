74b2fbf82ee80a0c3de9e011ca5defe0dbe7e00d
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2025-03-18 21:11:11 -0600

    Carpeta de Vue

---
+++ fundamentos/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Cerezos</h1>
+  <p>La flor de cerezo es una flor efímera de los cerezos que florece al comienzo de la primavera y 
+    puede ser de distintos colores y tonalidades, especialmente de color rosa pálido.</p>
+  
+  <img class="image" src="https://th.bing.com/th/id/R.f5c0b38bf1bf372efa69d081b68bbb98?rik=CATtMot1u8LDZg&pid=ImgRaw&r=0" alt="flor de cerezo">
+  <br>
+  <button class="btn ">Primer boton</button>
+
+</template>
+
+<style scoped>
+
+h1{
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-size:45px ;
+  color:rgb(27, 109, 109);
+
+
+}
+.btn{
+  border-radius: 15px;
+  background-color: rgb(58, 58, 161);
+  color: #fff;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.image{
+  width: 300px;
+  height: 200px;
+  border-radius: 15px;
+
+}
+</style>

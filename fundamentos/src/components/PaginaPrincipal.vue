b8aa1d67f80bee96bf01c053145a4ed330d3d006
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2025-03-20 22:05:40 -0600

    componente para el Card de postres

--- fundamentos/src/components/PaginaPrincipal.vue
+++ fundamentos/src/components/PaginaPrincipal.vue
@@ -4,6 +4,7 @@ import HeaderBar from './HeaderBar.vue';
 
 import Menu from './Menu.vue';
 import Carosel from './Carosel.vue';
+import cardPostres from './cardPostres.vue';
 
 </script>
 
@@ -13,44 +14,9 @@ import Carosel from './Carosel.vue';
   <!-- creamos un menu -->
   <Menu/>
  <!-- utilizamos un carrucel -->
- <!--  <Carosel/>
+  <Carosel/>
   <!-- un menu de potres -->
-  <div class="container my-5">
-    <div class="row " style="background-color: #9a928f09; height: 300px;">
-      <h3 class="text-center" style="font-family: 'Pincuk - Fuentes Sans' ;font-size: 30px;">Mejores Postres</h3>
-
-      <div class="col-md-4">
-        <div class="card" style="border-radius: 5%;">
-          <img style="border-radius: 5%; height: 200px;"  src="https://th.bing.com/th/id/OIP.J2ROZtcgXyEGv5b1KEsAbwHaE7?w=626&h=417&rs=1&pid=ImgDetMain" class="card-img-top " alt="Tiramizu">
-          <div class="card-img-overlay">
-            <p class="card-caption d-none d-md-block" style="color: #d8cfcfcf;text-align:center;font-size: 30px;font-family:George Round;">Tiramisú</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-4">
-        <div class="card" style="border-radius: 5%;">
-          <img style="border-radius: 5%; height: 200px; width: 120;"  src="https://th.bing.com/th/id/R.cb0447da9b3b804d353fd2df9a857c15?rik=pSTGoPQpmYpaTQ&riu=http%3a%2f%2fbizcocheando.com%2fwp-content%2fuploads%2f2021%2f03%2fCremaCatalana_7-1024x683.jpg&ehk=zKIcma7B0zvAqmlJX8Jj%2bf0Jc%2fCMCXRdstuta20MOEY%3d&risl=&pid=ImgRaw&r=0" class="card-img-top" alt="Tiramizu">
-          <div class="card-img-overlay">
-            <p class="card-caption d-none d-md-block" style="color: #fff;text-align: center;font-size: 30px;font-family:George Round;">Crema catalana</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-4">
-        <div class="card" style="border-radius: 5%;">
-          <img style="border-radius: 5%; height: 200px;"  src="https://th.bing.com/th/id/OIP.P3dPk1kvx6heWPsI9L-rcQHaEM?rs=1&pid=ImgDetMain" class="card-img-top" alt="Tiramizu">
-          <div class="card-img-overlay">
-            <p class="card-caption d-none d-md-block" style="color: #d8cfcfcf;text-align: center;font-size: 30px;font-family:George Round;">Cheesecake</p>
-          </div>
-        </div>
-        </div>
-        
-        </div>
-        <center><button type="button" class="btn btn-outline-primary" s>Destacados</button>
-        <button type="button" class="btn btn-outline-primary">Mas recientes</button>
-        <button type="button" class="btn btn-outline-primary">Mejores Vendidos</button></center>
-      </div>
+ <!--   <cardPostres/>
      <!-- seccion -->
   <section class="nosotros my-5">
     

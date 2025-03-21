454f71f9c56f00ce5f57d0d53c80943311814863
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2025-03-20 22:09:20 -0600

    componente para la seccion de textos e imagenes

--- fundamentos/src/components/PaginaPrincipal.vue
+++ fundamentos/src/components/PaginaPrincipal.vue
@@ -5,6 +5,7 @@ import HeaderBar from './HeaderBar.vue';
 import Menu from './Menu.vue';
 import Carosel from './Carosel.vue';
 import cardPostres from './cardPostres.vue';
+import seccion from './seccion.vue';
 
 </script>
 
@@ -16,30 +17,9 @@ import cardPostres from './cardPostres.vue';
  <!-- utilizamos un carrucel -->
   <Carosel/>
   <!-- un menu de potres -->
- <!--   <cardPostres/>
+  <cardPostres/>
      <!-- seccion -->
-  <section class="nosotros my-5">
-    
-    <div class="container">
-      <div class="row">
-        <h2 style="font-family: Berlin - Fuentes de Letras Redondas;" > Bienvenidos COFFE </h2>
-        <div class="col-md-6">
-          <p style="text-align: justify; color: rgb(7, 7, 92);font-size: 18px;">En COFFE, no solo
-            servimos café, sino que creamos momentos especiales.
-            Nos apasiona ofrecerte una experiencia única, con sabores auténticos y
-            una atmósfera acogedora. Desde nuestros granos seleccionados a mano hasta
-            las delicadas combinaciones de sabores, cada taza está hecha con amor y dedicación.</p>
-        </div>
-        <div class="col-md-6">
-          <img style=" height: 170px ;width: 450px;" class="img-fluid"
-            src="https://th.bing.com/th/id/R.6f5c50013936c54d16e3eb739dab06a4?rik=wFYawKzbTg5hgA&pid=ImgRaw&r=0"
-            alt="bebidas">
-
-
-        </div>
-      </div>
-    </div>
-  </section>
+  <seccion/>
   <!-- OFERTAS con row, card-->
 
   <section class="OFERTAS my-5">
---
+++ fundamentos/src/components/seccion.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="nosotros my-5">
+    
+    <div class="container">
+      <div class="row">
+        <h2 style="font-family: Berlin - Fuentes de Letras Redondas;" > Bienvenidos COFFE </h2>
+        <div class="col-md-6">
+          <p style="text-align: justify; color: rgb(7, 7, 92);font-size: 18px;">En COFFE, no solo
+            servimos café, sino que creamos momentos especiales.
+            Nos apasiona ofrecerte una experiencia única, con sabores auténticos y
+            una atmósfera acogedora. Desde nuestros granos seleccionados a mano hasta
+            las delicadas combinaciones de sabores, cada taza está hecha con amor y dedicación.</p>
+        </div>
+        <div class="col-md-6">
+          <img style=" height: 170px ;width: 450px;" class="img-fluid"
+            src="https://th.bing.com/th/id/R.6f5c50013936c54d16e3eb739dab06a4?rik=wFYawKzbTg5hgA&pid=ImgRaw&r=0"
+            alt="bebidas">
+
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>

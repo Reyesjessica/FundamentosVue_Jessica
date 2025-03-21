1619a86db4377480b621ce41b9f519e22f4c8035
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2025-03-20 22:12:35 -0600

    componente para los card de ofertas

---
+++ fundamentos/src/components/cardOfertas.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="OFERTAS my-5">
+    <h2 style="font-family:'Times New Roman', Times, serif; color:#320000;" class="text-center my-5"> OFERTAS</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+
+          <div class="card">
+            <img style="height: 250px;"
+              src="https://th.bing.com/th/id/OIP.ldsOqbm-2OlaBV01_HCE2QHaEo?rs=1&pid=ImgDetMain" class="card-img-top"
+              alt="">
+            <div class="card-body text-center">
+              <h2 style="color:#4A2C0F; font-family:George Round;"> Nueva bebida</h2>
+              <p style="text-align: justify; line-height: 2em;"> Una nueva combinacion de granos de cafe para mejorar el
+                sabor y presentacion </p>
+              <a href="#" class="btn btn-primary text-center"> Mas Informacion </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div style="height: 500px;" class="card">
+            <img style="height: 250px;"
+              src="https://th.bing.com/th/id/OIP.MXZNNAnlldgKSGWDo_06HAHaHX?rs=1&pid=ImgDetMain" class="card-img-top"
+              alt="">
+            <div class="card-body text-center">
+              <h2 style="font-family:George Round;color: #3E1404;"> Café Frappé</h2>
+              <p style="text-align: justify; line-height: 2em;"> ¡Un frappé es justo lo que necesitas! Con un rico sabor
+                y un dulce aroma. </p>
+              <a href="#" class="btn btn-primary text-center"> Mas Informacion </a>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img style="height:250px ;"
+              src="https://th.bing.com/th/id/OIP.NcfUfdhzTMIVJcUGYr1B0gHaLG?rs=1&pid=ImgDetMain" class="card-img-top"
+              alt="">
+            <div class="card-body text-center">
+              <h2 style="font-family: George Round; color:#3E1404; grpu"> Ice Latte</h2>
+              <p style="text-align: justify; line-height: 2em;"> Es una bebida deliciosa y refrescante hecha con café
+                espresso, leche fría y hielo. </p>
+              <a href="#" class="btn btn-primary text-center"> Mas Informacion </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>

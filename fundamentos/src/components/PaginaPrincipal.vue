73ac2571cacfa5baeba8c3b921d5780f697c29b0
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2025-03-20 22:01:15 -0600

    componente para el carosel

--- fundamentos/src/components/PaginaPrincipal.vue
+++ fundamentos/src/components/PaginaPrincipal.vue
@@ -3,6 +3,7 @@
 import HeaderBar from './HeaderBar.vue';
 
 import Menu from './Menu.vue';
+import Carosel from './Carosel.vue';
 
 </script>
 
@@ -12,44 +13,7 @@ import Menu from './Menu.vue';
   <!-- creamos un menu -->
   <Menu/>
  <!-- utilizamos un carrucel -->
- <div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-
-      <div class="carousel-item">
-        <img style=" height: 400px;"
-          src="https://th.bing.com/th/id/R.266142eac61bf6b22405777893798ea9?rik=FEi6%2bDuUNi4W0w&pid=ImgRaw&r=0/1920/500"
-          class="d-block w-100" alt="cafeteria">
-      </div>
-
-      <div class="carousel-item">
-        <img style=" height: 400px;"
-          src="https://images.unsplash.com/photo-1532003455308-dcf31d7182f6?mark=https:%2F%2Fimages.unsplash.com%2Fopengraph%2Flogo.png&mark-w=64&mark-align=top%2Cleft&mark-pad=50&h=630&w=1200&crop=faces%2Cedges&blend-w=1&blend=000000&blend-mode=normal&blend-alpha=10&auto=format&fit=crop&q=60&ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzIzMzE5ODkzfA&ixlib=rb-4.0.3"
-          class="d-block w-100" alt="Menu">
-      </div>
-
-      <div class="carousel-item active">
-        <img style=" height: 400px;"
-          src="https://www.recetasnestle.com.ec/sites/default/files/2021-12/tazas-con-tipos-de-cafe.jpg"
-          class="d-block w-100" alt="bebidas">
-      </div>
-
-      <div class="carousel-item">
-        <img style=" height: 400px;" src="https://img.freepik.com/foto-gratis/vista-aerea-varios-cafes_53876-146530.jpg"
-          class="d-block w-100" alt="bebidas">
-      </div>
-
-
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-
+ <!--  <Carosel/>
   <!-- un menu de potres -->
   <div class="container my-5">
     <div class="row " style="background-color: #9a928f09; height: 300px;">
@@ -201,30 +165,6 @@ import Menu from './Menu.vue';
 </template>
 
 <style scoped>
-.icon-button{
-    padding: 10px 20px;
-    font-size: 16px;
-    background-color: #cd966c;
-    color: white;
-    border: none;
-    border-radius: 40px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-}
-.icon-button i {
-margin-right: 10px;
-}
-body{
-    font-family: 'Times New Roman', Times, serif;
-    overflow-x: hidden;
-
-}
-#menu li > a{
-    font-size: 15px;
-    font-weight: bold;
-    color:black;
-}
 
 
 </style>
